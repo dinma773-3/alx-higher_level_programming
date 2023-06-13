94d46c9521c88877aca1c1dd4cd4b89c52b83b26
dinma773-3 <godsfavourchukwudi21@gmail.com>
2023-06-12 23:34:36 -0700

    Data Structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    leng = len(my_string)
+    j = 0
+    new_str = my_string[:]
+
+    for i in range(leng):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_str = new_str[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    return (new_str)
